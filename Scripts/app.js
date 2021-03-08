f92181cd9d7cb38fb41e833b6897e05beab050f4
Javid <j-n_01@hotmail.com>
2021-03-08 14:17:13 -0500

    first commit

---
+++ Scripts/app.js
@@ -0,0 +1,29 @@
+
+
+// Animate a Smooth Scrooling
+var scroll = new SmoothScroll('a[href*="#" ]', {
+    speed: 1200
+});
+
+
+// Scroll to Top Animation
+$(window).scroll(function () {
+    if ($(this).scrollTop() >= 100) {
+        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200); // Fade in the arrow
+    }
+    else {
+        $('#return-to-top').fadeOut(200); // Else fade out the arrow
+    }
+}
+);
+
+$('#return-to-top').click(function () {
+    // When arrow is clicked
+    $('body,html').animate({
+        scrollTop: 0 // Scroll to top of body
+    }
+        , 600);
+}
+
+);
